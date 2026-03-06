ce936fa1939ca8337b7675fc71c50c74f54361c6
F. Munroe Parlee <munroeparlee@gmail.com>
2026-03-05 18:09:11 -0800

    Refactor code-tutorial.py for clarity and structure

--- code-tutorial.py
+++ code-tutorial.py
@@ -1,25 +1,8 @@
+#imports
 import machine
 import utime
-#Sr.No.		Hex Code	Command to LCD instruction Register
-#1			01			Clear display screen
-#2			02			Return home
-#3			04			Decrement cursor (shift cursor to left)
-#4			06			Increment cursor (shift cursor to right)
-#5			05			Shift display right
-#6			07			Shift display left
-#7			08			Display off, cursor off
-#8			0A			Display off, cursor on
-#9			0C			Display on, cursor off
-#10			0E			Display on, cursor blinking
-#11			0F			Display on, cursor blinking
-#12			10			Shift cursor position to left
-#13			14			Shift the cursor position to the right
-#14			18			Shift the entire display to the left
-#15			1C			Shift the entire display to the right
-#16			80			Force cursor to the beginning ( 1st line)
-#17			C0			Force cursor to the beginning ( 2nd line)
-#18			38			2 lines and 5×7 matrix
 
+#interface definition
 rs = machine.Pin(16,machine.Pin.OUT) 	#rs: 	register select
 e = machine.Pin(17,machine.Pin.OUT)		#e: 	enable/disable changes
 d4 = machine.Pin(18,machine.Pin.OUT)	#d4-7: 	sends binary information through pins i.e. serial output
@@ -69,10 +52,32 @@ def clear_screen():
     rs.value(0)		#select command register
     send2LCD8(0x01) #clear screen
     rs.value(1)		#select input register
-#========definitions===========#
+#========reference information===========#
+#=display command information=#
+#Sr.No.		Hex Code	Command to LCD instruction Register
+#1			01			Clear display screen
+#2			02			Return home
+#3			04			Decrement cursor (shift cursor to left)
+#4			06			Increment cursor (shift cursor to right)
+#5			05			Shift display right
+#6			07			Shift display left
+#7			08			Display off, cursor off
+#8			0A			Display off, cursor on
+#9			0C			Display on, cursor off
+#10			0E			Display on, cursor blinking
+#11			0F			Display on, cursor blinking
+#12			10			Shift cursor position to left
+#13			14			Shift the cursor position to the right
+#14			18			Shift the entire display to the left
+#15			1C			Shift the entire display to the right
+#16			80			Force cursor to the beginning ( 1st line)
+#17			C0			Force cursor to the beginning ( 2nd line)
+#18			38			2 lines and 5×7 matrix
+
 #registers:
     # register 0: command input
     # register 1: letter and digit input
+
 #========working code==========#
 setUpLCD() 
 
@@ -98,4 +103,5 @@ for x in string: 	# runs the indented code once per character in the string surr
 #     utime.sleep_us(pause_length)
 
 #try changing the pause_length and string variables from the last code block
+
 
